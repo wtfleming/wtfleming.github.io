dfb72d48d209863521e16c00f77b80ae746729f2
Will Fleming <wfleming77@gmail.com>
2013-11-10 14:51:33 -0800

    msp430 blink LED post

---
+++ code/msp430-blink-led/main.c
@@ -0,0 +1,14 @@
+#include <msp430g2453.h>
+
+int main(void) {
+  WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
+  P1DIR = 0x01; // Set pin to output, 0b00000001
+
+  for (;;) {
+    volatile unsigned int i;
+    P1OUT ^= 0x01; // toggle LED1 (P1.0) on/off, 0b00000001
+    i = 50000;
+    do (i--);
+    while (i != 0);
+  }
+}
